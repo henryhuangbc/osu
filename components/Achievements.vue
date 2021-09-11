5d70b77ec534e56ecf249cdf8db29400dad63c7c
henryhuangbc <85907185+henryhuangbc@users.noreply.github.com>
2021-09-11 16:50:47 -0700

    Delete components directory

--- components/Achievements.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>Achievements</h1>
-</template>
-
-<script>
-export default {
-  name: 'Achievements'
-}
-</script>
--- components/Loading.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>{{ $t('general.loading') }}</h1>
-</template>
-
-<script>
-export default {
-  name: 'Loading',
-}
-</script>
